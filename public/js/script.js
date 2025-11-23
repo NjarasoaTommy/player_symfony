b4b0e0405c0aa8b75c48963cb5ba20a083b5fea5
Njarasoa Tommy <njarasoatommy@gmail.com>
2025-11-23 18:27:42 +0300

    Update the style of the overlay background behind the popup

--- public/js/script.js
+++ public/js/script.js
@@ -1,12 +1,13 @@
 function showFormModal(type){
     document.getElementsByClassName("overlay")[0].style.top = "0";
-    document.getElementsByClassName("overlay")[0].style.left = "0";
-    document.getElementsByClassName("overlay")[0].style.right = "0";
-    document.getElementsByClassName("overlay")[0].style.bottom = "0";
+    document.getElementsByClassName("overlay")[0].style.width = "100vw";
+    document.getElementsByClassName("overlay")[0].style.height = "100vh";
 }
 
 function closeFormModal(){
     document.getElementsByClassName("overlay")[0].style.top = "-100%";
+    document.getElementsByClassName("overlay")[0].style.width = "0vw";
+    document.getElementsByClassName("overlay")[0].style.height = "0vh";
 }
 
 function updatePlayer(id){
