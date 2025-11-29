10fd6aa4685393d63c36f89e1f99e703fc7b9dc1
Njarasoa Tommy <njarasoatommy@gmail.com>
2025-11-29 20:59:20 +0300

    Create the backend of the application

--- src/Controller/PlayerController.php
+++ src/Controller/PlayerController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -15,22 +16,9 @@ final class PlayerController extends AbstractController
 {
     #[Route('/', name: 'app_player')]
     public function index(
-        EntityManagerInterface $em,
         PlayerRepository $playerRepository,
-        Request $request
     ): Response
     {
-        $data = $request->request->all();
-        if(isset($data["add"])){
-            $player = new Player();
-            $player->setFirstname($data["firstname"]);
-            $player->setLastname($data["lastname"]);
-            $player->setPoste($data["poste"]);
-            $player->setNumber($data["number"]);
-
-            $em->persist($player);
-            $em->flush();
-        }
         $all_players = $playerRepository->findAll();
         return $this->render('player/index.html.twig', [
             'all_players' => $all_players,
@@ -56,6 +44,48 @@ final class PlayerController extends AbstractController
             $em->flush();
             $this->addFlash("success", "Player added successfully");
         }
+
+        else if(isset($data["update"])){
+            $player = $playerRepository->find($data["id"]);
+            // $player->setFirstname($data["firstname"]);
+            $player->setLastname($data["lastname"]);
+            $player->setPoste($data["poste"]);
+            $player->setNumber($data["number"]);
+
+            $em->persist($player);
+            $em->flush();
+        }
+
         return $this->redirectToRoute("app_player");
+    }
+    
+    #[Route('/delete', name: 'api_delete_player_by_id')]
+    public function DeleteById(
+        PlayerRepository $playerRepository,
+        Request $request,
+        EntityManagerInterface $em
+    ): Response
+    {
+        $id = $request->request->all()['id'];
+        $player = $playerRepository->find($id);
+        $em->remove($player);
+        $em->flush();
+
+        return $this->redirectToRoute("app_player");
+    }
+    
+    #[Route('/api/player/{id}', name: 'api_get_player_by_id')]
+    public function apiGetPlayerById(
+        PlayerRepository $playerRepository,
+        int $id
+    ): JsonResponse
+    {
+        $player = $playerRepository->find($id);
+        return $this->json([
+            'lastname' => $player->getLastname(),
+            // 'firstname' => $player->getFirstname(),
+            'poste' => $player->getPoste(),
+            'number' => $player->getNumber(),
+        ]);
     }
 }
